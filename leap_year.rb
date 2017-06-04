dc495961406a5ba82785b54eca88ee5aa359279a
lbias <306508354@qq.com>
2017-06-04 15:27:37 +0800

    fix

--- leap_year.rb
+++ leap_year.rb
@@ -1,7 +1,11 @@
 def is_leap_year?(year)
   if year % 4 == 0
     if year % 100 == 0
-      return false
+      if year % 400 == 0
+        return true
+      else
+        return false
+      end
     else
       return true
     end
